afaac61b6dfdc07ee5009f8812eb2bc3e37ea29f
Justin <justinhwang92@gmail.com>
2021-09-10 14:18:01 -0400

    Add Original code

--- sep7.js
+++ sep7.js
@@ -32,3 +32,26 @@ async function getAndGroupStuff() {
 }
 
 getAndGroupStuff();
+
+// let lib = require("./lib.js");
+
+// async function getAndGroupStuff() {
+//     let thingsToFetch = ['t1', 't2', 't3', 's1', 's2', 's3', 'm1', 'm2', 'm3', 't4'];
+//     let stuff = [];
+//     let ts, ms, ss;
+
+//     let promises = [];
+//     for (let thingToGet of thingsToFetch) {
+//         stuff.push(await lib.getPromise(thingToGet));
+//         console.log("Got a thing");
+//     }
+//     ts = await lib.groupPromise(stuff,"t");
+//     console.log("Made a group");
+//     ms = await lib.groupPromise(stuff,"m");
+//     console.log("Made a group");
+//     ss = await lib.groupPromise(stuff,"s");
+//     console.log("Made a group");
+//     console.log("Done");
+// }
+
+// getAndGroupStuff();
